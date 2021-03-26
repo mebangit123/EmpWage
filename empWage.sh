d7f56f8971a443aa60f4266ed9824b05c6b961b7
mebangit123 <mebannongrum333@gmail.com>
2021-03-26 15:46:00 +0530

    empWage1 added

--- empWage.sh
+++ empWage.sh
@@ -1,3 +1,12 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage Computation program on Master Branch"
 
+isPresent=1
+empCheck=$((RANDOM%2))
+
+if [ $isPresent -eq $empCheck ]
+then
+	echo "Present"
+else
+	echo "Absent"
+fi
